040beea526050ba9c3d26d0f55b3b8a2a5ba5041
Madeleine Lorås <madeleine.loras@gmail.com>
2025-03-14 14:46:12 +0100

    Added searchbar

--- src/routes/gamelibrary/+page.svelte
+++ src/routes/gamelibrary/+page.svelte
@@ -1,7 +1,8 @@
 <script>
   //TODO add database for borrowing games
+  //TODO
 </script>
 
-<h1>Lån et spill</h1>
+<h1>Lån et spill (kommer)</h1>
 
 <a class="nav-button" href="/"> ... tilbake </a>
--- src/routes/gamelist/+page.svelte
+++ src/routes/gamelist/+page.svelte
@@ -2,9 +2,10 @@
   import GameCard from "../gamelist/GameCard.svelte";
   import gamefile from "../gamelist/games.json";
   import LinkButton from "../Components/LinkButton.svelte";
-  import Sanityconnector from "./Sanityconnector.svelte";
-  let games = gamefile.games;
+  import SearchBar from "./SearchBar.svelte";
+  let games = $state(gamefile.games);
 
+  //GET data from sanity
   let PROJECT_ID = "x9czv3a6";
   let DATASET = "production";
   let QUERY = encodeURIComponent('*[_type == "games"]');
@@ -14,15 +15,39 @@
     const response = await fetch(SANITYURL);
     return await response.json();
   })();
+
+  //SEARCH bar
+  let searchinput = $state("Search...");
+
+  function searchedList(searchterm) {
+    let originalGameList = games;
+    if (searchterm == "") {
+      games = gamefile.games;
+    } else
+      games = games.filter((game) => {
+        console.log(game.title.startsWith(searchterm));
+        return game.title.includes(searchterm) == true;
+      });
+  }
 </script>
 
 <h1>Maddes spillbibliotek</h1>
 <LinkButton linkHref="" linkLabel="Tilbake" />
 
 <div>
+  Find a game:
+  <input
+    bind:value={() => searchinput, (v) => ((searchinput = v), searchedList(v))}
+  />
+</div>
+
+<div>
   {#await fetchData}
     <p>Loading games...</p>
   {:then data}
+    <div>
+      <GameList gamelist={data} />
+    </div>
     {#each data.result as game}
       <GameCard gamename={game.name} gameyear={game.year} gametype={""} />
     {/each}
